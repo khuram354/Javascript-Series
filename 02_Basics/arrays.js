d5c7dbfd58343ac6329168807075596dde32fa84
Khuram Shahzad <khurammirza354@gmail.com>
2023-09-25 11:31:50 +0500

    decreasing lenght of array

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -12,7 +12,20 @@ console.log(fruits); // []
 fruits.push("banana", "apple", "peach");
 console.log(fruits.length); // 3
 
+// When setting a property on a JavaScript array when the property is a valid array index and that index is outside the current bounds of the array, the engine will update the array's length property accordingly:
 fruits[5] = "mango";
 console.log(fruits[5]); // 'mango'
 console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
 console.log(fruits.length); // 6
+
+// Increasing the length extends the array by adding empty slots without creating any new elements — not even undefined.
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); // undefined
+
+// Decreasing the length property does, however, delete elements.
+fruits.length = 2;
+console.log(Object.keys(fruits)); // ['0', '1']
+console.log(fruits.length); // 2
