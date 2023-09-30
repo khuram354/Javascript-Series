0a16b799fa03a358c155001d5536a0101e70cdb2
Khuram Shahzad <khurammirza354@gmail.com>
2023-09-30 22:35:11 +0500

    changes

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -1,6 +1,9 @@
 //  The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+
 //  In JavaScript, arrays aren't primitives but are instead Array objects with the following core characteristics:
+
 //  JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead.)
+
 //  JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the last element is at the value of the array's length property minus 1.
 //  JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
 
